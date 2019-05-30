05b8597ef228fa60a2aa765190ce3831391b92ee
sniukalov <sniukalov@luxoft.com>
2019-05-30 09:52:35 +0300

    Fix UTs

--- src/components/application_manager/test/policy_handler_test.cc
+++ src/components/application_manager/test/policy_handler_test.cc
@@ -1750,7 +1750,8 @@ TEST_F(PolicyHandlerTest, OnSnapshotCreated_UrlNotAdded) {
       .WillRepeatedly(SetArgReferee<1>(test_data));
   policy_handler_.OnSnapshotCreated(msg, retry_delay_seconds, timeout_exchange);
 #else   // EXTERNAL_PROPRIETARY_MODE
-  policy_handler_.OnSnapshotCreated(msg);
+  policy_handler_.OnSnapshotCreated(msg,
+                                    policy::PTUIterationType::DefaultIteration);
 #endif  // EXTERNAL_PROPRIETARY_MODE
 }
 
@@ -1828,7 +1829,8 @@ TEST_F(PolicyHandlerTest, DISABLED_OnSnapshotCreated_UrlAdded) {
 #endif  // PROPRIETARY_MODE
 
   EXPECT_CALL(*mock_policy_manager_, OnUpdateStarted());
-  policy_handler_.OnSnapshotCreated(msg);
+  policy_handler_.OnSnapshotCreated(msg,
+                                    policy::PTUIterationType::DefaultIteration);
 }
 #endif  // EXTERNAL_PROPRIETARY_MODE
 
